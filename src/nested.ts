2d28c364ecf886d970b6296db7df36ba5b3e5d07
JuanSaq <jsaquino04@gmail.com>
2024-02-27 19:26:35 -0500

    implemented removeQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -48,7 +48,9 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    return questions.filter(
+        (question: Question): boolean => id !== question.id
+    );
 }
 
 /***
