b2cc7f1a44c4bcd03f0b7b5dfe050c7eb1743b4c
JuanSaq <jsaquino04@gmail.com>
2024-02-27 19:11:37 -0500

    implemented findQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -37,7 +37,10 @@ export function findQuestion(
     questions: Question[],
     id: number
 ): Question | null {
-    return null;
+    const foundQuestion = questions.find(
+        (question: Question): boolean => question.id === id
+    );
+    return foundQuestion ? foundQuestion : null;
 }
 
 /**
