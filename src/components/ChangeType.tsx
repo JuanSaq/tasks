fd47be087b1af5eb8ba927cbf1b3fed52a32758d
JuanSaq <jsaquino04@gmail.com>
2024-03-05 19:04:31 -0500

    implemented RevealAnswer and ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,26 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [question, setQuestion] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function flipQuestion(): void {
+        setQuestion(
+            question === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+    function renderQuestion(): string {
+        return question === "short_answer_question"
+            ? "Short Answer"
+            : "Multiple Choice";
+    }
+    return (
+        <div>
+            <Button onClick={flipQuestion}>Change Type</Button>
+            <div>Current QuestionType: {renderQuestion()}</div>
+        </div>
+    );
 }
