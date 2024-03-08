fd364e50a912722f89f0ac2534602688d0cb988b
JuanSaq <jsaquino04@gmail.com>
2024-03-07 20:41:10 -0500

    implemented EditMode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,51 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [isEditMode, setEditMode] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setStudent] = useState<boolean>(true);
+
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <div>
+                <Form.Check
+                    type={"switch"}
+                    id={"edit-mode-check"}
+                    label={"Edit Mode"}
+                    checked={isEditMode}
+                    onChange={updateEditMode}
+                />
+            </div>
+            <div>
+                <Form.Group controlId={"editMode"} hidden={!isEditMode}>
+                    <Form.Check
+                        type={"checkbox"}
+                        id={"is-student-check"}
+                        label={"Student"}
+                        checked={isStudent}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setStudent(event.target.checked)}
+                        disabled={!isEditMode}
+                    />
+                    <Form.Control
+                        value={name}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setName(event.target.value)}
+                        disabled={!isEditMode}
+                    />
+                </Form.Group>
+            </div>
+            <p>
+                {name} is {isStudent ? "" : "not"} a student
+            </p>
         </div>
     );
 }
